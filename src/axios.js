9c79224b4ace49b6a11f76035f435e0b566ea888
Ulmie <ulmie1257@gmail.com>
2023-06-27 03:13:06 +0300

    first commit7

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const makeRequest = axios.create({
   baseURL: "https://ulmie-api-production.up.railway.app/api/",
-  withCredentials: true,
+  // withCredentials: true,
 
 })
 
